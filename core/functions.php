60fc70d79d75021325271d631bb942077f9c6a91
Carla Saberola <82490633+carla011001-afk@users.noreply.github.com>
2021-05-08 13:52:30 +0800

    Add files via upload
    gumawa po ako sir ng functions (createUser) and (uidExists) ang problem po . Triny ko po mag insert or mag add ng user pero po di po sya pumapasok sa database po namin.
    and yung isa pa po kapag po mag lo'login triny ko po na mag login sa hindi existing na user account pero dumedirekta pa din po sa customer interface na dapat po maga failed kase po hindi pa register yung acc. po . di po nachecheck if existing or not ang isang acc.

--- core/functions.php
+++ core/functions.php
@@ -60,4 +60,48 @@ function getItem($conn, $table, $column, $value){
         return $arr;               //this is the return value
         mysqli_stmt_close($stmt);  //close the mysqli_statement
     
+} 
+
+function createUser($conn, $username, $password, $usertype, $firstname, $lastname, $address, $user_contact){
+
+        $err;
+        $sql ="INSERT INTO 'users' ('username', 'password', 'usertype', 'firstname', 'lastname', 'address', 'user_contact') VALUES (?,?,?,?,?,?,?);";
+
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql)){
+            return false;
+            exit();
+        }
+            mysqli_stmt_bind_param($stmt, "sssssss", );
+            mysqli_stmt_execute($stmt);
+
+            mysqli_stmt_close($stmt);
+            return true;
+           
 }
+
+function uidExists($conn, $username, $password, $firstname, $lastname, $address, $user_contact){
+            $err;
+            $sql="SELECT * FROM 'users' WHERE 'username' = ? AND 'password' = ? ;"; 
+            $stmt = mysqli_stmt_init($conn);
+
+            if (!mysql_stmt_prepare($stmt, $sql)){
+                header("location: customer.php?error=stmtfailed");
+                exit();
+            }
+
+            mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+            mysqli_stmt_execute($stmt);
+
+            $resultData = mysqli_stmt_get_result($stmt);
+
+            if($row = mysqli_fetch_assoc($resultData)){
+                return $row;
+            }
+            else{
+                $err = false;
+            return $err;
+            }
+            mysqli_stmt_close($stmt);
+} 
