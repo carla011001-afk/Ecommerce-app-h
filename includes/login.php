eb86abe2fffc2714baeba587eddacd71b5a44a34
Carla Saberola <82490633+carla011001-afk@users.noreply.github.com>
2021-05-12 12:16:09 +0800

    Add files via upload

--- includes/login.php
+++ includes/login.php
@@ -1,19 +1,21 @@
- <?php
-if(isset($_POST['submit'])){
-include_once "core/db_conn.php";
-include_once "core/functions.php";
+<?php
+								if(isset($_POST['login'])){ 
+									include_once "core/db_conn.php";
+									include_once "core/functions.php";
 
-$username = htmlentities($_POST['username']);
-$password = htmlentities($_POST['password']); 
- 
-		if(uidExists($conn, $username, $password) !==false){
-			header("location: ../customer.php?logged=yes");
-			exit();
-		}
-		else{
-			header("location: ../login.php?logged=no");
-			exit();
-		}
+									$username = htmlentities($_POST['username']);
+									$password = htmlentities($_POST['password']);
 
-}  
-?>
+								if(uidExists($conn, $username, $password) !==false){
+									echo "You are logged in!";
+								}else{
+									echo "nahhh";
+								}
+
+									
+
+								}
+
+
+							?>
+				
