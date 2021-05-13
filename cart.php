84d26e4bb8f8c209afa82f4e0c79a937c949327e
jennelyn-nual <77817175+jennelyn-nual@users.noreply.github.com>
2021-05-13 12:44:50 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -41,9 +41,9 @@
 						<div class="social-icons pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href=""><i class="fa fa-facebook"></i></a></li>
-								<li><a href=""><i class="fa fa-twitter"></i></a></li>
-								<li><a href=""><i class="fa fa-linkedin"></i></a></li>
-								<li><a href=""><i class="fa fa-dribbble"></i></a></li>
+								
+								
+								
 								<li><a href=""><i class="fa fa-google-plus"></i></a></li>
 							</ul>
 						</div>
@@ -74,14 +74,14 @@
 							</div>
 							
 							<div class="btn-group">
-								<!--<button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-									DOLLAR
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu">
-									<li><a href="">Canadian Dollar</a></li>
-									<li><a href="">Pound</a></li>
-								</ul>-->
+								
+									
+									
+								
+								
+									
+									
+								
 							</div>
 						</div>
 					</div>
@@ -89,10 +89,10 @@
 						<div class="shop-menu clearfix pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href=""><i class="fa fa-user"></i> Account</a></li>
-								<li><a href=""><i class="fa fa-star"></i> Wishlist</a></li>
+								
 								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
 								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+								
 							</ul>
 						</div>
 					</div>
@@ -122,25 +122,25 @@
 										<li><a href="checkout.html">Checkout</a></li> 
 										<li><a href="cart.html" class="active">Cart</a></li> 
 										<li><a href="login.html">Login</a></li> 
+                                    
+                                
+								
+
+
+										
                                     </ul>
                                 </li> 
-								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
-                                </li> 
-								<li><a href="404.html">404</a></li>
+								
 								<li><a href="contact-us.html">Contact</a></li>
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3">
-						<div class="search_box pull-right">
-							<input type="text" placeholder="Search"/>
-						</div>
+					
+						
+							
+						
 					</div>
-				</div>
+				
 			</div>
 		</div><!--/header-bottom-->
 	</header><!--/header-->
@@ -275,10 +275,10 @@
 
 	<section id="do_action">
 		<div class="container">
-			<div class="heading">
-				<h3>What would you like to do next?</h3>
-				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
-			</div>
+			
+				
+				
+			
 			<div class="row">
 				<div class="col-sm-6">
 					<div class="chose_area">
@@ -335,7 +335,7 @@
 								<input type="text">
 							</li>
 						</ul>
-						<a class="btn btn-default update" href="">Get Quotes</a>
+						
 						<a class="btn btn-default check_out" href="">Continue</a>
 					</div>
 				</div>
@@ -391,18 +391,18 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-2">
-						<div class="single-widget">
-							<h2>Quock Shop</h2>
-							<ul class="nav nav-pills nav-stacked">
-								<li><a href="">T-Shirt</a></li>
-								<li><a href="">Mens</a></li>
-								<li><a href="">Womens</a></li>
-								<li><a href="">Gift Cards</a></li>
-								<li><a href="">Shoes</a></li>
-							</ul>
-						</div>
-					</div>
+					
+						
+							
+							
+								
+								
+								
+								
+								
+							
+						
+					
 					<div class="col-sm-2">
 						<div class="single-widget">
 							<h2>Policies</h2>
@@ -427,16 +427,10 @@
 							</ul>
 						</div>
 					</div>
-					<div class="col-sm-3 col-sm-offset-1">
-						<div class="single-widget">
-							<h2>About Shopper</h2>
-							<form action="#" class="searchform">
-								<input type="text" placeholder="Your email address" />
-								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
-								<p>Get the most recent updates from <br />our site and be updated your self...</p>
-							</form>
-						</div>
-					</div>
+					
+							
+						
+					
 					
 				</div>
 			</div>
