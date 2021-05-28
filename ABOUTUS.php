bb2135b4bac2d9107fdd8da57fc6a8ad5f5b7f83
jennelyn-nual <77817175+jennelyn-nual@users.noreply.github.com>
2021-05-28 12:16:51 +0800

    Add files via upload

---
+++ ABOUTUS.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>aboutus|contactus</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<style></style>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.php" class="navbar-brand">LGA E-CLOTHING</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span> Products</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-modal-note"></span> About us</a></li>
+			</ul>
+		</div>
+</div>
+</div>
+<h1>LGA-CLOTHING</h1>
+ <div class="container">
+	
+	<p>WE OFFER THE BEST AND HIGH QUALITY SHIRTS AND VAPE!</p>
+	<div class="contact-box">
+		<div class="contact-left">
+			<h3>Sent your request</h3>
+			<form>
+				<div class="input-row">
+					<div class="input-group">
+						<label>Name</label>
+						<input type="text" placeholder="">
+						
+					</div>
+					<div class="input-group">
+						<label>Phone</label>
+						<input type="text" placeholder="">
+						
+					</div>
+					
+				</div>
+				<div class="input-row">
+					<div class="input-group">
+						<label>Email</label>
+						<input type="Email" placeholder="">
+						
+					</div>
+					
+					
+				</div>
+				<label>Message</label>
+				<textarea rows="5" placeholder="Your Message"></textarea>
+
+				<button type="submit">Send</button>
+			</form>
+		</div>
+			<div class="contact-right">
+				<h3>Reach Us</h3>
+				
+				<table>
+					<tr>
+						<td>Email:</td>
+						<td>lgaclothing@gmail.com</td>
+					</tr>
+					<tr>
+						<td>Phone:</td>
+						<td>09063779184</td>
+					</tr>
+					<tr>
+						<td>Address:</td>
+						<td>4506 ZONE|FELICIDAD SUBD SUCGAD, POLANGUI,ALBAY PHILIPPINES</td>
+					</tr>
+				</table>
+			</div>
+			
+				
+			</div>
+			<footer class="footer">
+				<div class="container">
+					<div class="rows">
+						<div class="footer-col">
+							<h4>Services</h4>
+							<ul>
+								<li>T-shirts<a href="#"></a></li>
+								<li>Vape<a href="#"></a></li>
+							</ul>
+							
+						</div>
+							<div class="footer-col">
+							<h4>Policies</h4>
+							<ul>
+								<li>Terms of use<a href="#"></a></li>
+								<li>Privacy Policy<a href="#"></a></li>
+								<li>Refund Policy<a href="#"></a></li>
+							</ul>
+							
+						</div>
+						<div class="footer-col">
+							<h4>Get Help</h4>
+							<ul>
+								<li>FAQ's<a href="#"></a></li>
+								<li>Shipping<a href="#"></a></li>
+								<li>Returns<a href="#"></a></li>
+								<li>Order status<a href="#"></a></li>
+								<li>Payment Options<a href="#"></a></li>
+							
+							</ul>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+				
+			</footer>
+</body>
+</html>
